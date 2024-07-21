b22fa4686660446f43c5d225e299da7b90beeec7
SmailliwArrillaga <135678771+SmailliwArrillaga@users.noreply.github.com>
2024-07-21 15:47:23 -0300

    Update proyectoFinalStreamlit.py
    actualizacion de la ruta del CSV

--- dashboardfitness/proyectoFinalStreamlit.py
+++ dashboardfitness/proyectoFinalStreamlit.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide", page_title="Fitness Dashboard")
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv('C:/Users/admin/OneDrive/Documentos/aprende_programando/dataAnalitycsConPython/dashboardfitness/dailyActivity_merged.csv')
+    df = pd.read_csv('dailyActivity_merged.csv')
     df['ActivityDate'] = pd.to_datetime(df['ActivityDate'])
     return df
 
